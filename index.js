a9e9371eeb486fee05ce0d3348bd59df6c7767d0
kato912 <89004444+kato912@users.noreply.github.com>
2023-12-25 23:01:30 +0700

    Update index.js

--- index.js
+++ index.js
@@ -8,7 +8,7 @@ const fs = require('fs');
 const cookieParser = require('cookie-parser');
 const axios = require('axios');
 
-mongoose.connect('mongodb+srv://admin:adminadmin123@dbuser.ifbo4sx.mongodb.net/?retryWrites=true&w=majority', {
+mongoose.connect('mongodb+srv://admin:adminadmin123@cluster0.teg5tvb.mongodb.net/?retryWrites=true&w=majority', {
     useNewUrlParser: true
 });
 
