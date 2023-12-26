2f190590b4421ed1f8222739a82161d139a2ada7
kato912 <89004444+kato912@users.noreply.github.com>
2023-12-26 23:57:20 +0700

    Update qrcodeGEN.js

--- qrcodeGEN.js
+++ qrcodeGEN.js
@@ -5,7 +5,7 @@ const randomstring = require('randomstring');
 module.exports = async (req, res, next) => {
     try {
         const xxyxx = randomstring.generate(10);
-        const Urladdpoint = `/${xxyxx}`;
+        const Urladdpoint = `https://point-test.onrender.com/${xxyxx}`;
         const url = Urladdpoint;
         const outputFilePath = 'public/img/qrcode.png';
 
