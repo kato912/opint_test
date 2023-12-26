c096f0d456fdc2226f96608a55d8e533e8c1b0fb
kato912 <89004444+kato912@users.noreply.github.com>
2023-12-26 23:53:14 +0700

    Update index.js

--- index.js
+++ index.js
@@ -130,28 +130,7 @@ const readFileAsync = promisify(fs.readFile);
             res.status(500).send('Error888 Vip');
           }
           else{
-            res.send(`
-            
-                        <!DOCTYPE html>
-            <html lang="en">
-            <head>
-                <meta charset="UTF-8">
-                <meta name="viewport" content="width=device-width, initial-scale=1.0">
-                <title>admin</title>
-            </head>
-            <body>
-                <h1>Yourqrcode</h1>
-
-                <img src="${qrCodeurl}" alt="">
-            </div>
-            <form action="/dtqrcode" method="POST">
-                <button>del</button>
-            </form>
-            <a href="/Home">Home</a>
-            </body>
-            </html>
-            
-            `);
+            res.send('adminn');
           }
         });
         });
